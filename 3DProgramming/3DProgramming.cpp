7d43e25cd9906b215c0c0030aea5f57caaa53051
Figix <1202hyunjin@naver.com>
2022-09-17 15:22:06 +0900

    [HJ] 3D프로그래밍 3주차 과제201934033

--- 3DProgramming/3DProgramming.cpp
+++ 3DProgramming/3DProgramming.cpp
@@ -1,7 +1,6 @@
 ﻿#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
-
 #pragma comment(lib, "OpenGL32")
 
 static void error_callback(int error, const char* description)
@@ -15,6 +14,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 }
 
+void colorTrans(int angle);
 
 int main(void)
 {
@@ -22,7 +22,7 @@ int main(void)
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(640, 640, "Simple example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -49,15 +49,19 @@ int main(void)
         glClearColor(.0f, 0.0f, 0.0f, 0.1f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glLineWidth(20);
         glBegin(GL_LINE_STRIP);
-        glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-        glVertex3f(0.0f, 1.0f, 0.0f);
-        glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-        glVertex3f(1.0f, -1.0f, 0.0f);
-        glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-        glVertex3f(-1.0f, -1.0f, 0.0f);
-        glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-        glVertex3f(0.0f, 1.0f, 0.0f);
+        // 호도법의 각 = 육식분법의 각 * pi / 180
+        double radius = 1;
+        for (int i = 0; i < 360; i++) {
+            double theta = i * 3.141592 / 180;
+            double x = radius * cos(theta);
+            double y = radius * sin(theta);
+            colorTrans(i);
+            glVertex3f(x, y, 0.0f);
+        }
+        
+
         glEnd();
 
         glfwSwapBuffers(window);
@@ -67,4 +71,31 @@ int main(void)
     glfwTerminate();
     
     exit(EXIT_SUCCESS);
+}
+
+void colorTrans(int angle) {
+    angle = angle % 180;
+    switch (angle / 25) {
+        case 0:     //빨강
+            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+            break;
+        case 1:     //주황
+            glColor4f(1.0f, 0.2f, 0.0f, 1.0f);
+            break;
+        case 2:     //노랑
+            glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+            break;
+        case 3:     //초록
+            glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+            break;
+        case 4:     //파랑
+            glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+            break;
+        case 5:     //남색
+            glColor4f(0.0f, 0.02f, 1.0f, 1.0f);
+            break;
+        default:    //보라 6,7이 몫으로 나오는데 둘다 보라로 만듬
+            glColor4f(0.4f, 0.0f, 1.0f, 1.0f);
+            break;
+    }
 }
