fea5fbbd17d1642853f7ee1af6d9a334b6bff995
Figix <1202hyunjin@naver.com>
2022-11-22 05:25:36 +0900

    [HJ] 힘겹게 과제 완료

--- Lecture10/Lecture10.cpp
+++ Lecture10/Lecture10.cpp
@@ -1,33 +1,273 @@
-﻿#include <iostream>
+﻿#include <chrono>
+#include <thread>
+#include <string>
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <iostream>
 #include "MuSoenMath.h"
 #define PI 3.141592
 
 using namespace std;
 
-void main()
+#pragma comment(lib, "OpenGL32")
+
+using namespace std;
+
+chrono::system_clock::time_point startRenderTimePoint;
+chrono::duration<double> renderDuration;
+
+GLFWwindow* window;
+bool isFirstFrame = true;
+
+
+struct Vertex
 {
-    std::cout << "Hello World!\n";
-    vec3 v;
-    v = vec3(2, 4, 1);
+    vec3 pos;
+    float r, g, b, a;
+};
 
-    mat3 Translate, Rotate, Scale;
-    Translate = mat3(1, 0, 3
-                    ,0, 1, 5
-                    ,0, 0, 1);
-    Rotate = mat3(float(cos(1/6*PI)), float(sin(1 / 6 * PI)), 0
-                 , float(-sin(1 / 6 * PI)), float(cos(1 / 6 * PI)), 0
-                 ,0, 0, 1);
-    Scale = mat3(2, 0, 0
-                ,0, 2, 0
-                ,0, 0, 1);
+struct Transform
+{
+    mat3 translate;
+    mat3 scale;
+    mat3 rotation;
+};
 
-    vec3 p = v * Translate * Rotate * Scale;   // 3,5 이동 | 30도 회전 | 2배 확대
-    p.print();
-    vec3 pp = v.calculate(Translate).calculate(Rotate).calculate(Scale);   // 3,5 이동 | 30도 회전 | 2배 확대
-    pp.print();
 
-    vec3 trans = v * Translate;
-    trans.print();
-    vec3 tranS = v.calculate(Translate);
-    tranS.print();
+Vertex star[5];             // static mesh
+Vertex transformedStar[5];  //화면에 그릴 오망성
+Vertex circle[360];             // static mesh 
+Vertex transformedCircle[360];  // 화면에 그려질 원
+
+Transform transform;  //world 행렬이 될 transform
+
+//<문제>////////전역변수 쓰는곳////////////////////////////////////////////////////////////
+float rotation = 0.0f;
+float rotationDir = 1.0f;
+float translateX = 0.0f;
+float translateY = 0.0f;
+float translateDir = 1.0f;
+float scale = 1.0f;
+float scaleDir = 1.0f;
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void Init();
+void Update();
+void Release();
+
+static void error_callback(int error, const char* description);
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+int main(void);
+
+
+void Init()
+{
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(720, 720, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glfwSwapInterval(1);
+
+    startRenderTimePoint = chrono::system_clock::now();
+
+
+    //object생성부
+     /// Star(오망성) 생성
+    int i = 0;
+    for (float theta = 0; theta < 360; theta += 72)
+    {
+
+        star[i].pos.x = -sin(radians(theta)) * 0.5f;
+        star[i].pos.y = cos(radians(theta)) * 0.5f;
+        star[i].pos.z = 1.0f;
+
+        star[i].r = 0.3f;
+        star[i].g = 0.0f;
+        star[i].b = theta / 360.0f;
+        star[i].a = 0.7f;
+
+
+        transformedStar[i] = star[i];
+
+        i++;
+    }
+
+    // 원 생성
+    for (int theta = 0; theta < 360; theta++)
+    {
+        circle[theta].pos.x = -sin(radians((float)theta)) * 0.5;
+        circle[theta].pos.y = cos(radians((float)theta)) * 0.5;
+        circle[theta].pos.z = 1.0f;
+
+        circle[theta].r = 0.3f;
+        circle[theta].g = 0.0f;
+        circle[theta].b = (float)theta / 360.0f;
+        circle[theta].a = 0.7f;
+
+        transformedCircle[theta] = circle[theta];
+    }
+
+
+    //트랜스폼 초기화 (기본형 제공)
+    transform.translate = mat3(
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1
+    );
+    transform.rotation = mat3(
+        cos(radians(0.0f)), -sin(radians(0.0f)), 0,
+        sin(radians(0.0f)), cos(radians(0.0f)), 0,
+        0, 0, 1
+    );
+    transform.scale = mat3(
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1
+    );
+
+
+}
+
+void Release()
+{
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+void Update()
+{
+    while (!glfwWindowShouldClose(window))
+    {
+        //Update로직
+        //<문제>//////////////////////////////////////////////////////////////////////////////////
+
+        //1. translate 를 프레임당 오른쪽으로 0.001씩 누적시켜서 물체를 이동해보세요.
+        //2. Rotation 을 프레임당 1도씩 누적시켜서 물체를 회전시켜보세요.
+        //3. Scale은 초당 0.01씩 최대 1.3배까지 늘어났다가 0.7배까지 줄어들도록 만드시오 (반복)
+        //   (1.3배 이상이 되면 줄어들고 0.7배 이하가 되면 다시 늘어나게 만드시오)
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        /*회전*/
+        transform.rotation = mat3(
+            cos(radians(rotation)), -sin(radians(rotation)), 0,
+            sin(radians(rotation)), cos(radians(rotation)), 0,
+            0, 0, 1
+        );
+        rotation = rotation + 1.0f * rotationDir;
+        if (rotation >= 360) { rotation = 0.0f; }
+
+        /*이동*/
+        transform.translate = mat3(
+            1, 0, 0,    //x
+            0, 1, 0,    //y
+            translateX, translateY, 1
+        );
+        translateX = translateX + (0.001f * translateDir);
+
+        if (translateX * translateX >= 0.25f) {
+            translateDir = -translateDir;
+            rotationDir = -rotationDir;
+        }
+        cout << translateX;
+
+        /*스케일*/
+        transform.scale = mat3(
+            scale, 0, 0,
+            0, scale, 0,
+            0, 0, 1
+        );
+        scale = scale + (0.01 * scaleDir / 1); //60프레임 기준으로 프로그램이 돌아가기 때문에
+        if (scale >= 1.3 || scale <= 0.7) { scaleDir = scaleDir * -1.0f; }
+        for (int i = 0; i < 360; i++)
+        {
+            transformedCircle[i].pos = circle[i].pos * transform.scale * transform.rotation * transform.translate;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            transformedStar[i].pos = star[i].pos * transform.scale * transform.rotation * transform.translate;
+        }
+
+
+
+        //색 초기화
+        glClearColor(.0f, 0.0f, 0.0f, 0.1f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //선두께
+        glLineWidth(7.0f);
+        //오망성 그리기
+        glBegin(GL_LINE_STRIP);
+
+        int a = 0;
+        for (int i = 0; i <= 10; i = i + 2) { // 0->2->4->1->3->0
+            a = i % 5;
+            glColor4f(transformedStar[a].r, transformedStar[a].g, transformedStar[a].b, transformedStar[a].a);
+            glVertex3f(transformedStar[a].pos.x, transformedStar[a].pos.y, 0.0f);
+        }
+
+        //원그리기
+        glBegin(GL_LINE_STRIP);
+        for (int theta = 0; theta < 360; theta++)
+        {
+            glColor4f(transformedCircle[theta].r, transformedCircle[theta].g, transformedCircle[theta].b, transformedCircle[theta].a);
+            glVertex3f(transformedCircle[theta].pos.x, transformedCircle[theta].pos.y, 0.0f);
+        }
+        glEnd();
+
+
+
+        //화면 스왑
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        //렌더시간 측정
+        renderDuration = chrono::system_clock::now() - startRenderTimePoint;    //렌더링하는데 걸린 시간 init마지막->update안 계산과 그리기 종료후 측정 확인
+        startRenderTimePoint = chrono::system_clock::now();                     //재측정 시작 구간
+
+        float fps = 1.0 / renderDuration.count();
+        if (isFirstFrame == true)
+        {
+            isFirstFrame = false;
+            continue;
+        }
+        if (renderDuration.count() < (1.0f / 60.0f)) //60프레임보다 더 빠를시 조건문 실행
+            this_thread::sleep_for(chrono::milliseconds((int)(((1.0f / 60.0f) - renderDuration.count()) * 1000)));
+        //60프레임 단위보다 더 작은 단위를 빼서 그 차만큼 멈춰서 60프레임 기준으로 맞춤
+        string fps_s = "FPS : " + to_string(fps);
+        cout << fps_s << endl;
+
+    }
+}
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(void)
+{
+    Init();
+    Update();
+    Release();
+
+    exit(EXIT_SUCCESS);
 }
